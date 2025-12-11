9ae9db6829221615fb850884c15431c5a3abffea
igortestett <igortorresti@gmail.com>
2025-12-11 16:32:21 -0300

    Refactor HelloWorld component with animations

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,46 +1,76 @@
-import { Composition } from "remotion";
-import { HelloWorld, myCompSchema } from "./HelloWorld";
-import { Logo, myCompSchema2 } from "./HelloWorld/Logo";
+import { spring } from "remotion";
+import {
+  AbsoluteFill,
+  interpolate,
+  Sequence,
+  useCurrentFrame,
+  useVideoConfig,
+} from "remotion";
+import { Logo } from "./HelloWorld/Logo";
+import { Subtitle } from "./HelloWorld/Subtitle";
+import { Title } from "./HelloWorld/Title";
+import { z } from "zod";
+import { zColor } from "@remotion/zod-types";
 
-// Each <Composition> is an entry in the sidebar!
+export const myCompSchema = z.object({
+  titleText: z.string(),
+  titleColor: zColor(),
+  logoColor1: zColor(),
+  logoColor2: zColor(),
+});
 
-export const RemotionRoot: React.FC = () => {
+export const HelloWorld: React.FC<z.infer<typeof myCompSchema>> = ({
+  titleText: propOne,
+  titleColor: propTwo,
+  logoColor1,
+  logoColor2,
+}) => {
+  const frame = useCurrentFrame();
+  const { durationInFrames, fps } = useVideoConfig();
+
+  // Animate from 0 to 1 after 25 frames
+  const logoTranslationProgress = spring({
+    frame: frame - 25,
+    fps,
+    config: {
+      damping: 100,
+    },
+  });
+
+  // Move the logo up by 150 pixels once the transition starts
+  const logoTranslation = interpolate(
+    logoTranslationProgress,
+    [0, 1],
+    [0, -150],
+  );
+
+  // Fade out the animation at the end
+  const opacity = interpolate(
+    frame,
+    [durationInFrames - 25, durationInFrames - 15],
+    [1, 0],
+    {
+      extrapolateLeft: "clamp",
+      extrapolateRight: "clamp",
+    },
+  );
+
+  // A <AbsoluteFill> is just a absolutely positioned <div>!
   return (
-    <>
-      <Composition
-        // You can take the "id" to render a video:
-        // npx remotion render HelloWorld
-        id="HelloWorld"
-        component={HelloWorld}
-        durationInFrames={150}
-        fps={30}
-        width={1920}
-        height={1080}
-        // You can override these props for each render:
-        // https://www.remotion.dev/docs/parametrized-rendering
-        schema={myCompSchema}
-        defaultProps={{
-          titleText: "Welcome to Remotion",
-          titleColor: "#000000",
-          logoColor1: "#91EAE4",
-          logoColor2: "#86A8E7",
-        }}
-      />
-
-      {/* Mount any React component to make it show up in the sidebar and work on it individually! */}
-      <Composition
-        id="OnlyLogo"
-        component={Logo}
-        durationInFrames={150}
-        fps={30}
-        width={1920}
-        height={1080}
-        schema={myCompSchema2}
-        defaultProps={{
-          logoColor1: "#91dAE2" as const,
-          logoColor2: "#86A8E7" as const,
-        }}
-      />
-    </>
+    <AbsoluteFill style={{ backgroundColor: "white" }}>
+      <AbsoluteFill style={{ opacity }}>
+        <AbsoluteFill style={{ transform: `translateY(${logoTranslation}px)` }}>
+          <Logo logoColor1={logoColor1} logoColor2={logoColor2} />
+        </AbsoluteFill>
+        {/* Sequences can shift the time for its children! */}
+        <Sequence from={35}>
+          <Title titleText={propOne} titleColor={propTwo} />
+        </Sequence>
+        {/* The subtitle will only enter on the 75th frame. */}
+        <Sequence from={75}>
+          <Subtitle />
+        </Sequence>
+      </AbsoluteFill>
+    </AbsoluteFill>
   );
 };
