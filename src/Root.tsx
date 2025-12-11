e0cadb4bec8f39e1f587aba13458f4aa70bb6837
igortestett <igortorresti@gmail.com>
2025-12-11 17:39:11 -0300

    Refactor VideoLongo component with metadata calculation
    Updated VideoLongo component to use automatic duration calculation.

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,7 +1,8 @@
 import { Composition } from "remotion";
 import { HelloWorld, myCompSchema } from "./HelloWorld";
 import { Logo, myCompSchema2 } from "./HelloWorld/Logo";
-import { VideoLongo } from './VideoLongo/Composition';
+
+// Import único e correto para o VideoLongo e sua função de metadata
 import { VideoLongo, calculateVideoLongoMetadata } from './VideoLongo/Composition';
 
 // Each <Composition> is an entry in the sidebar!
@@ -43,21 +44,20 @@ export const RemotionRoot: React.FC = () => {
           logoColor2: "#86A8E7" as const,
         }}
       /> 
-      {/* ^^^ ADICIONEI ESTE FECHAMENTO QUE FALTAVA */}
 
-<Composition
-  id="VideoLongo"
-  component={VideoLongo}
-  durationInFrames={150} // Valor inicial irrelevante, será sobrescrito
-  fps={30}
-  width={1920}
-  height={1080}
-  // LIGUE A FUNÇÃO AQUI:
-  calculateMetadata={calculateVideoLongoMetadata} 
-  defaultProps={{
-    audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
-    imagens: [
-      { url: "https://picsum.photos/seed/1/1920/1080", duracaoEmSegundos: 5 },  
+      {/* Novo VideoLongo com cálculo automático de duração */}
+      <Composition
+        id="VideoLongo"
+        component={VideoLongo}
+        durationInFrames={150} // Valor padrão (será ignorado se calculateMetadata funcionar)
+        fps={30}
+        width={1920}
+        height={1080}
+        calculateMetadata={calculateVideoLongoMetadata} 
+        defaultProps={{
+          audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+          imagens: [
+            { url: "https://picsum.photos/seed/1/1920/1080", duracaoEmSegundos: 5 },
           ]
         }}
       />
